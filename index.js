75a8eae310afc751f9e4bc50d67d7290b814758d
Daniel Williams <danielwil621@gmail.com>
2021-04-03 13:35:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
+  var copy = Object.assign({}, object);
   delete object.key;
   return object;
 }
